b8cee2850de49c780d7d8894ede8d188ad86fc10
Spyke Lionel <social.lionel@gmail.com>
2025-02-28 16:15:31 +0100

    primitive calculator

---
+++ index.js
@@ -0,0 +1,115 @@
+// what is a fn?
+// types of functions in js.
+function namedFunction() {
+  console.log("Hello from named function");
+}
+
+const arrowFunction = () => {
+  console.log("Hello from arrow function");
+};
+
+const anonymousFunction = function () {
+  console.log("Hello from anonymous function");
+};
+
+const addAnonymous = function (num1, num2) {
+  return num1 + num2;
+};
+// start or begin
+// get num1
+// get num2
+// set sum = num1 + num2
+// print sum
+// end
+function addNamed(num1, num2) {
+  return num1 + num2;
+}
+
+const addArrow = (num1, num2) => num1 + num2;
+const solutionToAdd = addArrow(1, 2);
+// index starts from ?
+
+// we have 2+ numbers
+const sum = function (numbers) {
+  // number of times we want the loop to run
+  // types of loops
+  // 1. for
+  // 2. while
+  // 3. do-while
+  // get length of numbers
+  const length = numbers.length;
+  // numbers = [7,2,3,4,5]
+  //  0,1,2,3,4
+  // lenght = 5
+  // start
+  // set sum = 0
+  // get numbers
+  // get length of numbers.
+  // set counter = 0
+  // count from counter to lenght
+  // set sum = numbers[counter] + sum
+  // increment counter (counter = counter + 1)
+  // end loop
+  // print sum;
+  // end
+  let sum = 0;
+  for (let i = 0; i < length; i++) {
+    sum = sum + numbers[i];
+  }
+  return sum;
+};
+// const sumOfnumbers = sum([1, 2, 3, 3, 3, 4]);
+// console.log(sumOfnumbers);
+
+// use a while loop and a do-while to calculate the sum, multiply, power, and factorial.
+// handle the case where the exponent is less than 0.
+// const multiply = function () {};
+
+// power of a number. x = a^n
+const power = function (base, exponent) {
+  let power = 1;
+  // check the base condition. i.e. when exponent = 0
+  if (exponent == 0) {
+    return power;
+  }
+  if (exponent < 0) {
+    // interns should handle this.
+    return;
+  }
+  // 2^3 = 2 * 2 * 2 = 8
+  // base = 2
+  // exponent = 3
+  for (let i = 0; i < exponent; i++) {
+    power = power * base;
+  }
+
+  // first run, power = 1 * 2  = 2
+  // second run, power = 2 * 2 = 4
+  //  thirdrun, power = 4 * 2 = 8
+
+  return power;
+};
+
+// const powerOf2 = power(2, 3);
+// console.log(powerOf2);
+// factorial of 3 = 3*2*1
+const factorial = function (n) {
+  let fact = 1;
+  if (n == 0 || n == 1) {
+    return fact;
+  }
+  if (n < 0) {
+    return "Number n, is negative.";
+  }
+  for (let i = n; i > 0; i--) {
+    fact = fact * i;
+  }
+
+  // let n = 3
+  // first run; fact = 1*3 = 3;
+  // sec run; fact = 3*2 = 6;
+  // third run; fact = 6 * 1
+  return fact;
+};
+console.log(factorial(3));
+// print 1 to 10
